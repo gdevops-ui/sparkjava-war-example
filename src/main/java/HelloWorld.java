0dbb8be650869de7ac99241bd1586a11cfe1837d
gdevops-ui <82902695+gdevops-ui@users.noreply.github.com>
2021-04-22 18:31:34 -0500

    requirement

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello", (req, res) -> "Hi devops");
+		get("/hello", (req, res) -> "Hi devops test111");
 	}
 }
